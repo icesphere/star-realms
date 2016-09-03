afb0eded2fa1a96415d3c228a76d7471bc24bc87
icesphere <icesphere12@gmail.com>
2016-09-03 09:39:52 -0600

    add united ships to bot strategies

--- src/main/java/org/smartreaction/starrealms/model/players/bots/strategies/EconomyStrategy.java
+++ src/main/java/org/smartreaction/starrealms/model/players/bots/strategies/EconomyStrategy.java
@@ -18,6 +18,7 @@ import org.smartreaction.starrealms.model.cards.ships.blob.*;
 import org.smartreaction.starrealms.model.cards.ships.machinecult.*;
 import org.smartreaction.starrealms.model.cards.ships.starempire.*;
 import org.smartreaction.starrealms.model.cards.ships.tradefederation.*;
+import org.smartreaction.starrealms.model.cards.ships.united.*;
 import org.smartreaction.starrealms.model.players.Player;
 
 public class EconomyStrategy implements BotStrategy {
@@ -135,6 +136,71 @@ public class EconomyStrategy implements BotStrategy {
                 return 30;
             }
             return 35;
+        }
+
+        //United
+        if (card instanceof AllianceFrigate) {
+            if (deck < 3) {
+                return 5;
+            }
+            return 20;
+        } else if (card instanceof AllianceTransport) {
+            if (deck == 1) {
+                return 45;
+            } else if (deck == 2) {
+                return 30;
+            } else if (deck == 3) {
+                return 15;
+            }
+        } else if (card instanceof AssaultPod) {
+            if (deck == 1) {
+                return 5;
+            } else if (deck == 2) {
+                return 15;
+            }
+            return 40;
+        } else if (card instanceof BlobBot) {
+            if (deck <= 2) {
+                return 35;
+            } else if (deck == 3) {
+                return 25;
+            }
+            return 10;
+        } else if (card instanceof CoalitionFreighter) {
+            if (deck == 1) {
+                return 90;
+            } else if (deck == 2) {
+                return 65;
+            } else if (deck == 3) {
+                return 40;
+            }
+            return 20;
+        } else if (card instanceof CoalitionMessenger) {
+            if (deck == 1) {
+                return 50;
+            } else if (deck == 2) {
+                return 35;
+            } else if (deck == 3) {
+                return 20;
+            }
+            return 10;
+        } else if (card instanceof TradeStar) {
+            if (deck == 1) {
+                return 35;
+            } else if (deck == 2) {
+                return 25;
+            } else if (deck == 3) {
+                return 15;
+            }
+        } else if (card instanceof UnityFighter) {
+            if (deck == 1) {
+                return 60;
+            } else if (deck == 2) {
+                return 40;
+            } else if (deck == 3) {
+                return 20;
+            }
+            return 10;
         }
 
         //Blob

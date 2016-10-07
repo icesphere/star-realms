6ccd8fe17f3e4f83d3a611e07b40a1da894042d6
icesphere <icesphere12@gmail.com>
2016-10-07 06:22:30 -0600

    fixes for Rule mission

--- src/main/java/org/smartreaction/starrealms/model/cards/missions/united/Rule.java
+++ src/main/java/org/smartreaction/starrealms/model/cards/missions/united/Rule.java
@@ -1,37 +1,30 @@
 package org.smartreaction.starrealms.model.cards.missions.united;
 
 import org.smartreaction.starrealms.model.cards.Faction;
-import org.smartreaction.starrealms.model.cards.bases.Base;
 import org.smartreaction.starrealms.model.cards.missions.Mission;
 import org.smartreaction.starrealms.model.players.Player;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Rule extends Mission {
     public Rule() {
         name = "Rule";
         objectiveText = "Have bases from two or more factions in play";
-        rewardText = "";
+        rewardText = "Acquire a card of cost 3 or less for free and put it into your hand";
     }
 
     @Override
     public boolean isMissionCompleted(Player player) {
 
-        Map<Faction, List<Base>> basesInPlayByFaction = new HashMap<>();
+        Set<Faction> factionsWithBaseInPlay = new HashSet<>();
 
         player.getBases().forEach(c -> {
             Set<Faction> factions = c.getFactions();
-            factions.forEach(f -> {
-                List<Base> cards = basesInPlayByFaction.get(f);
-                if (cards == null) {
-                    cards = new ArrayList<>();
-                }
-                cards.add(c);
-                basesInPlayByFaction.put(f, cards);
-            });
+            factions.forEach(factionsWithBaseInPlay::add);
         });
 
-        return basesInPlayByFaction.keySet().stream().anyMatch(f -> basesInPlayByFaction.get(f).size() >= 2);
+        return player.getBases().size() >=2 && factionsWithBaseInPlay.size() >= 2;
     }
 
     @Override
